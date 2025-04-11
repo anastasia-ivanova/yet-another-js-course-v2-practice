225a98554ccafb62dbc4da26fb9c47e5ccfdc080
Anastasia Ivanova <ivanidzzza@gmail.com>
2025-04-11 14:43:31 +0300

    fixing comments

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,7 +9,7 @@ test('Test 1: Verify login with valid credentials', async ({ page }) => {
   await page.locator('[data-test="login-submit"]').click();
   await expect(page).toHaveURL('/account');
   await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
-  await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');
+  await expect(page.locator('[data-test="nav-menu"]')).toContainText(process.env.USER_NAME);
 });
 
 test('Test 2: Verify user can view product details', async ({ page }) => {
@@ -23,6 +23,8 @@ test('Test 2: Verify user can view product details', async ({ page }) => {
 });
 
 test('Test 3: Verify user can add product to cart', async ({ page }) => {
+  const alert = page.getByRole('alert', { name: 'Product added to shopping cart.' });
+  const productQuantity = page.locator('[data-test="product-quantity"]');
   await page.goto('/');
   //Click on the product "Slip Joint Pliers"
   await page.locator(productCardByName, { hasText: 'Slip Joint Pliers' }).click();
@@ -35,20 +37,20 @@ test('Test 3: Verify user can add product to cart', async ({ page }) => {
   //Click "Add to Cart" button.
   await page.locator('[data-test="add-to-cart"]').click();
   //Verify alert message is visible
-  await expect(page.getByRole('alert', { name: 'Product added to shopping cart.' })).toBeVisible();
+  await expect(alert).toBeVisible();
   //Verify alert message text is "Product added to shopping cart".
-  await expect(page.getByRole('alert', { name: 'Product added to shopping cart.' })).toHaveText('Product added to shopping cart.');
+  await expect(alert).toHaveText('Product added to shopping cart.');
   //Verify alert disappears in 8 seconds.
-  await expect(page.getByRole('alert', { name: 'Product added to shopping cart.' })).not.toBeVisible({ timeout: 8000 });
+  await expect(alert).not.toBeVisible({ timeout: 8000 });
   //Verify cart icon in navigation shows quantity = 1.
   await expect(page.locator('[data-test="nav-cart"]')).toContainText('1');
   //Click on the cart icon in the navigation.
   await page.locator('[data-test="nav-cart"]').click();
   //Verify URL is https://practicesoftwaretesting.com/checkout.
   await expect(page).toHaveURL(process.env.WEB_URL + '/checkout');
-  await expect(page.locator('[data-test="product-quantity"]')).toBeVisible();
+  await expect(productQuantity).toBeVisible();
   //Verify the number of products in the cart table equals 1.
-  await expect(page.locator('[data-test="product-quantity"]')).toHaveValue('1');
+  await expect(productQuantity).toHaveValue('1');
   //Verify product title in the cart is "Slip Joint Pliers".
   await expect(page.locator('[data-test="product-title"]')).toContainText('Slip Joint Pliers');
   //Verify "Proceed to Checkout" button is visible.
